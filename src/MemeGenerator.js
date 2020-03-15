f0610dd37e3a3e5090553b95b98e6678420a352a
ltsimps <ltsimpso@me.com>
2020-03-15 11:58:43 -0400

    first commit

---
+++ src/MemeGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div> Meme </div>;
+  }
+}
+
+export default MemeGenerator;
